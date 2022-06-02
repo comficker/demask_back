ba28d46c090747a87b026d9988f2c4b93731b7c8
comficker <comficker@gmail.com>
2022-06-02 22:13:58 +0700

    init

--- app/management/commands/generate_rarity.py
+++ app/management/commands/generate_rarity.py
@@ -14,14 +14,14 @@ class Command(BaseCommand):
             total = contract.assets.count()
             traits = contract.traits.all()
             for trait in traits:
-                if trait.rarity == 0:
+                if trait.rarity == 1:
                     trait.rarity = trait.assets.count() / total
                     trait.save()
                     print(trait.id)
             if len(traits) > 0:
                 assets = contract.assets.all()
                 for asset in assets:
-                    if asset.rarity == 0:
+                    if asset.rarity == 1:
                         asset.rarity = asset.traits.aggregate(Sum('rarity'))['rarity__sum'] / asset.traits.count()
                         asset.save()
                         print(asset.id)
